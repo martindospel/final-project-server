0f8333b7a0b71232b92259b44e2d08e7f42fc742
Salt Dev <saltdev@Salts-MBP.localdomain>
2022-07-18 17:23:55 +0200

    day1

---
+++ db/db.js
@@ -0,0 +1,7 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = process.env.MONGODB_URI;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  client.close();
+});
