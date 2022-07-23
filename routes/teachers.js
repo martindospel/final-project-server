8c68fd274757554ff3c27cacf6de97bfee646b10
Mori <sayedmurtazamuttahary@gmail.com>
2022-07-23 12:17:43 +0200

    removed extra imports

--- routes/teachers.js
+++ routes/teachers.js
@@ -1,7 +1,6 @@
 const { Teacher } = require('../db');
 const express = require('express');
 const router = express.Router();
-const { v4: uuid } = require('uuid');
 
 // get one teacher
 router.get('/:uuid', async (req, res) => {
