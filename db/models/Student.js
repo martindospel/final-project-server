a7498e564254d98ad7a8bff48b494dae68d80957
Mori <sayedmurtazamuttahary@gmail.com>
2022-07-26 11:02:00 +0200

    added edit student profile route

--- db/models/Student.js
+++ db/models/Student.js
@@ -1,35 +1,39 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+const { v4: uuid } = require("uuid");
 const Schema = mongoose.Schema;
-const { v4: uuid } = require('uuid')
 
 const StudentSchema = new Schema({
-    uuid: {
-        type: String,
-        default: uuid
-    },
-    studentName: {
-        type: String,
-        default: false,
-        required: true
-    },
-    dob: {
-        type: String,
-        required: true
-    },
-    classUuid: {
-        type: String,
-        required: true
-    },
-    assessments: {
-        type: [Object],
-        default: []
-    },
-    gender: {
-        type: String,
-        required: false
-    },
-})
+  uuid: {
+    type: String,
+    default: uuid,
+  },
+  studentName: {
+    type: String,
+    default: false,
+    required: true,
+  },
+  dob: {
+    type: String,
+    required: true,
+  },
+  classUuid: {
+    type: String,
+    required: true,
+  },
+  assessments: {
+    type: [Object],
+    default: [],
+  },
+  gender: {
+    type: String,
+    required: false,
+  },
+  emergencyContact: {
+    type: Object,
+    default: {},
+  },
+});
 
-const Student = mongoose.model("Student", StudentSchema)
+const Student = mongoose.model("Student", StudentSchema);
 
 module.exports = Student;
